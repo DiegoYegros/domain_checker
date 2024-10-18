13744a89425bbc66193b50e2894b2855c997e61f
Diego Yegros <diegoyegrosa@gmail.com>
2024-10-18 15:51:08 -0300

    Added Daily Report feature, improved messages and documentation

--- README.md
+++ README.md
@@ -6,19 +6,27 @@ docker build -t domain-checker .
 ```
 
 To run the container, you can use the following command, just replace the example values:
+
+### Must specify
+SUBDOMAINS
+BOT_TOKEN
+CHAT_ID
+
 #### Default Values
 RETRIES=3
-SLEEP_TIME=60
-DELAY_BEFORE_RETRYING=60
+SLEEP_TIME=60 // seconds
+DELAY_BEFORE_RETRYING=60 // seconds
+REPORT_INTERVAL=86400 // seconds
 
 ```bash
 docker run -d \
     --name domain-checker \
-    -e BOT_TOKEN='your_bot_token_here' \
-    -e CHAT_ID='your_chat_id_here' \
-    -e RETRIES='retries_amount_here' \
-    -e SLEEP_TIME='sleep_time_here' \
-    -e DELAY_BEFORE_RETRYING='delay_time_here' \
-    -e SUBDOMAINS='example1.com,example2.com,example3.com' \
+    -e BOT_TOKEN=your_bot_token_here \
+    -e CHAT_ID=your_chat_id_here \
+    -e RETRIES=retries_amount_here \
+    -e REPORT_INTERVAL=your_report_interval_here \
+    -e SLEEP_TIME=sleep_time_here \
+    -e DELAY_BEFORE_RETRYING=delay_time_here \
+    -e SUBDOMAINS=example1.com,example2.com,example3.com \
     domain-checker
 ```
--- domain-checker.py
+++ domain-checker.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import logging
+import datetime
 from time import sleep
 
 logging.basicConfig(level=logging.INFO, 
@@ -10,38 +11,81 @@ logging.basicConfig(level=logging.INFO,
                         logging.StreamHandler()  # Sends log to stdout to see logs with docker logs.
                     ])
 
+
+# Initialize counters
+total_requests = 0
+successful_requests = 0
+failed_requests = 0
+last_report_time = datetime.datetime.now()
+
 # Telegram Bot Token and Chat ID
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
-TELEGRAM_API_URL = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
-
 SUBDOMAINS = os.getenv('SUBDOMAINS', '').split(',')
 RETRIES = int(os.getenv('RETRIES', 3))
 SLEEP_TIME = int(os.getenv('SLEEP_TIME', 60))
+REPORT_INTERVAL = int(os.getenv('REPORT_INTERVAL', 86400))  # 24hs in seconds
 DELAY_BEFORE_RETRYING = int(os.getenv('DELAY_BEFORE_RETRYING', 60))
-print(f"Sleep time: {SLEEP_TIME}, Retries: {RETRIES}, Delay before retrying: {DELAY_BEFORE_RETRYING}, Subdomains: {SUBDOMAINS}")
+
+TELEGRAM_API_URL = f'https://api.telegram.org/bot{BOT_TOKEN}/sendMessage'
+
+
 def check_subdomain(subdomain, retries=RETRIES, delay=DELAY_BEFORE_RETRYING):
     """Check if the subdomain is accessible."""
+    global total_requests, successful_requests, failed_requests
     attempt = 0
+
+    if not subdomain.startswith(('http://', 'https://')):
+        subdomain = f'http://{subdomain}'
+
     while attempt < retries:
+        total_requests += 1
         try:
-            response = requests.get(f'http://{subdomain}', timeout=5)
+            response = requests.get(subdomain, timeout=5)
             if response.status_code == 200:
                 logging.info(f'Subdomain {subdomain} is UP.')
+                successful_requests += 1
                 return True
             else:
                 logging.warning(f'Subdomain {subdomain} is DOWN with status code {response.status_code}.')
         except requests.exceptions.RequestException as e:
             logging.error(f'Subdomain {subdomain} is DOWN. Exception: {str(e)}')
+            failed_requests += 1
         attempt += 1
         logging.info(f"Retrying subdomain {subdomain} in {delay} seconds... (Attempt {attempt}/{retries})")
         sleep(delay)
     logging.error(f"Subdomain {subdomain} is DOWN after {retries} attempts.")
+    failed_requests += 1
     return False
+
+def send_report():
+    global total_requests
+    global successful_requests
+    global failed_requests
+    message = (f"📊 Report:\n"
+               f"Total Requests: {total_requests}\n"
+               f"✅ Successful: {successful_requests}\n"
+               f"❌ Failed: {failed_requests}"
+               f"Sending next report in {REPORT_INTERVAL} seconds.")
+    payload = {
+        'chat_id': CHAT_ID,
+        'text': message
+    }
+    try:
+        response = requests.post(TELEGRAM_API_URL, data=payload)
+        if response.status_code == 200:
+            logging.info('Successfully sent report.')
+        else:
+            logging.error(f'Failed to send report. Status code: {response.status_code}')
+        total_requests = 0
+        successful_requests = 0
+        failed_requests = 0
+    except requests.exceptions.RequestException as e:
+        logging.error(f'Failed to send report. Exception: {str(e)}')
 
 def send_alert(subdomain):
     """Send an alert to the user via Telegram."""
-    message = f"⚠️ Alert: Subdomain {subdomain} is DOWN!"
+    message = f"⚠️ Alert: {subdomain} is DOWN!"
     payload = {
         'chat_id': CHAT_ID,
         'text': message
@@ -57,7 +101,12 @@ def send_alert(subdomain):
 
 def main(subdomains, retries=RETRIES, sleep_time=SLEEP_TIME, delay=DELAY_BEFORE_RETRYING):
     """Main function to check subdomains and send alerts."""
+    global last_report_time
     while True:
+        current_time = datetime.datetime.now()
+        if last_report_time is not None and (current_time - last_report_time).total_seconds() >= REPORT_INTERVAL:
+            send_report()
+            last_report_time = current_time
         for subdomain in subdomains:
             if not check_subdomain(subdomain, retries, delay):
                 send_alert(subdomain)
@@ -69,4 +118,6 @@ if __name__ == "__main__":
         logging.error("No subdomains provided. Exiting.")
         exit(1)
     logging.info("Subdomain checker started.")
+    logging.info(f"Sleep time: {SLEEP_TIME}, Retries: {RETRIES}, Delay before retrying: {DELAY_BEFORE_RETRYING}, Subdomains: {SUBDOMAINS}, Report Interval: {REPORT_INTERVAL}")
+
     main(SUBDOMAINS)
